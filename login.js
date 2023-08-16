496c3afe749c31442762282820440dc699a051e0
mind-set09 <141085830+mind-set09@users.noreply.github.com>
2023-08-15 20:16:09 -0400

    Update login.js

--- login.js
+++ login.js
@@ -7,27 +7,24 @@ document.addEventListener('DOMContentLoaded', function () {
     const username = loginForm.elements.username.value;
     const password = loginForm.elements.password.value;
 
-    // Simulate API call for login using JSONPlaceholder's /users endpoint
-    const response = await fetch('https://jsonplaceholder.typicode.com/users', {
-      method: 'GET'
+    // Simulate API call for login using login.php endpoint
+    const response = await fetch('backend/login.php', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: `username=${username}&password=${password}`
     });
 
     if (response.ok) {
-      const users = await response.json();
-      const user = users.find(u => u.username === username && u.email === password);
-
-      if (user) {
-        // Successful login, redirect to dashboard
-        window.location.href = 'dashboard.html';
-      } else {
-        // Show error message
-        const errorMessage = document.createElement('p');
-        errorMessage.textContent = 'Invalid username or password.';
-        errorMessage.classList.add('error-message');
-        loginForm.appendChild(errorMessage);
-      }
+      // Successful login, redirect to dashboard
+      window.location.href = 'dashboard.html';
     } else {
-      console.error('Failed to fetch users data.');
+      // Show error message
+      const errorMessage = document.createElement('p');
+      errorMessage.textContent = 'Invalid username or password.';
+      errorMessage.classList.add('error-message');
+      loginForm.appendChild(errorMessage);
     }
   });
 });
